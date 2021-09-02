623372f562099825177b585c15a287bc2c29c1c9
Theo Nicolas <theonicolas87@gmail.com>
2021-09-02 13:03:32 -0300

    Aprendizado testes

---
+++ EstudosAutomação/main.js
@@ -0,0 +1,4 @@
+//para comentar no javascritp usar duas barras -> //
+var welcome= 'Hello Javascript - Automação Full Stack from QA Ninja'
+        document.getElementById('welcome').append('Hello Javascript - Automação Full Stack from QA Ninja')
+        console.log("welcome") //ESTE CÓD Foi construido no html, e colado em JS após a integração de ambos arquivos
